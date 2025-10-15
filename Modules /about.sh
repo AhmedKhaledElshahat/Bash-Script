f72764f347ca32c5ded8240cc6f0830600fcdaae
Ahmed Khaled <ahmed8khaled24@gmail.com>
2025-10-15 18:13:32 +0300

    Create about.sh

---
+++ Modules /about.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+echo "=============================================="
+echo " Bash User & Group Management Tool"
+echo " Created by: Ahmed Khaled"
+echo " Description: Manage users & groups easily!"
+echo "=============================================="
+
+
+about() {
+    clear
+    echo "=============================================="
+    echo "  Bash User & Group Management Tool"
+    echo " Created by: Ahmed Khaled"
+    echo " Description: Manage users & groups easily!"
+    echo "=============================================="
+}
